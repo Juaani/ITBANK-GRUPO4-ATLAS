bc8a259c03b6af7f27a5ce2945ffa95ab6d8eec0
AUCB21 <aucontrerasb@gmail.com>
2022-06-21 10:14:05 -0300

    mejorado de muestra de cotizaciones, emprolijado de textos

--- api.js
+++ api.js
@@ -12,7 +12,7 @@ let ids = [
 
   const d = new Date();
   let day = d.getDate();
-  let month = d.getMonth();
+  let month = d.getMonth() + 1;
   let hours = d.getHours();
   let minute= d.getMinutes();
 
@@ -21,39 +21,96 @@ function api_request(){
     fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
     .then(response=>response.json())
     .then(data=>{
-        for(let i in ids){
-            console.log(data[i].casa.nombre);
-            let contenido= document.getElementById(ids[i]);
-            let titulo= document.getElementById("titulocoti")  
-            titulo.innerHTML= `Cotizaciones`    
-            contenido.innerHTML= `<div id="infonueva" class="card bg-light mb-3" style="max-width: 18rem; background-color: #1d71b8"">
-            <div class="card-header bg-secondary text-white">${data[i].casa.nombre}</div>
-          <div class="row justify-content-md-center" style="display: flex;
-          width: 100%;">
-          <div class="col-6" style="font-size:80%">
-            Precio Venta:
-          </div>
-          <div class="col-6" style="font-size:80%">
-          Precio Compra:
-          </div>
-          </div>
-          <div class="row justify-content-md-center">
-          <div class="col-6">
-           <h4> $${data[i].casa.venta}</h4>
-          </div>
-          <div class="col-6">
-          <h4> $${data[i].casa.compra}</h4>
-          </div>
-          </div> 
-    
-          <div class="card-footer text-muted" style="font-size:60%">
-          Última actualización: ${day}/${month} ${hours}:${minute} hs
+      for(let i in ids){
+          // console.log(data[i].casa.nombre);
+          let contenido= document.getElementById(ids[i]);
+          let titulo= document.getElementById("titulocoti")  
+          titulo.innerHTML= `Cotizaciones`    
+          contenido.innerHTML= `<div id="infonueva" class="card bg-light mb-3" style="max-width: 18rem; background-color: #1d71b8"">
+          <div class="card-header bg-secondary text-white tituloPrecios" style="text-align: center;">${data[i].casa.nombre}</div>
+        <div class="row justify-content-md-center" style="display: flex;
+        width: 100%;;">
+        <div class="col-6" style="font-size:80%">
+          Precio Venta:
         </div>
+        <div class="col-6" style="font-size:80%">
+        Precio Compra:
+        </div>
+        </div>
+        <div class="row justify-content-md-center">
+        <div class="col-6">
+         <h4 style=" padding: 5%"> $${data[i].casa.venta}</h4>
+        </div>
+        <div class="col-6">
+        <h4 style=" padding: 5%"> $${data[i].casa.compra}</h4>
+        </div>
+        </div> 
+  
+        <div class="card-footer text-muted" style="font-size:60%">
+        Última actualización: ${day}/${month} ${hours}:${minute} hs
+      </div>
 
+      </div>
+        `;
+        if(data[i].casa.compra == "No Cotiza"){
+          contenido.innerHTML =  `<div id="infonueva" class="card bg-light mb-3" style="max-width: 18rem; background-color: #1d71b8"">
+          <div class="card-header bg-secondary text-white tituloPrecios" style="text-align: center;">
+          ${data[i].casa.nombre}
+          </div>
+        <div class="row justify-content-md-center" style="display: flex;
+        width: 100%;;">
+        <div class="col-6" style="font-size:80%">
+          Precio Venta:
         </div>
-          `;      
-        }      
-    })
+        <div class="col-6" style="font-size:80%">
+        Precio Compra:
+        </div>
+        </div>
+        <div class="row justify-content-md-center">
+        <div class="col-6">
+         <h4 style=" padding: 5%"> $${data[i].casa.venta}</h4>
+        </div>
+        <div class="col-6">
+        <h4 style=" padding: 5%">-</h4>
+        </div>
+        </div> 
+  
+        <div class="card-footer text-muted" style="font-size:60%">
+        Última actualización: ${day}/${month} ${hours}:${minute} hs
+      </div>
+
+      </div>
+        `
+        };
+        if(data[i].casa.venta == 0){
+          contenido.innerHTML =  `<div id="infonueva" class="card bg-light mb-3" style="max-width: 18rem; background-color: #1d71b8"">
+          <div class="card-header bg-secondary text-white tituloPrecios" style="text-align: center;">${data[i].casa.nombre}</div>
+        <div class="row justify-content-md-center" style="display: flex;
+        width: 100%;;">
+        <div class="col-6" style="font-size:80%">
+          Precio Venta:
+        </div>
+        <div class="col-6" style="font-size:80%">
+        Precio Compra:
+        </div>
+        </div>
+        <div class="row justify-content-md-center">
+        <div class="col-6">
+         <h4 style=" padding: 5%">-</h4>
+        </div>
+        <div class="col-6">
+        <h4 style=" padding: 5%">$${data[i].casa.compra}</h4>
+        </div>
+        </div> 
+  
+        <div class="card-footer text-muted" style="font-size:60%">
+        Última actualización: ${day}/${month} ${hours}:${minute} hs
+      </div>
+
+      </div>
+        `
+        };
+    }})
 }
 
 function main(){
