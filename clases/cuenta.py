b9f50861f461963af3a2c593432981488162f49a
Facundo Barrionuevo <facundo.barrionuevo@hotmail.com>
2022-07-17 13:08:04 -0300

    commit to delete functions

--- clases/cuenta.py
+++
@@ -1,8 +0,0 @@
-class Cuenta:
-    def __init__(self, limite_extraccion_diario, limite_transferencia_recibida, monto, costo_transferencias, saldo_descubierto_disponible):
-        self.__limite_extraccion_diario = limite_extraccion_diario
-        self.__limite_transferencia_recibida = limite_transferencia_recibida
-        self.__monto = monto
-        self.__costo_transferencias = costo_transferencias
-        self.__saldo_descubierto_disponible = saldo_descubierto_disponible
-
